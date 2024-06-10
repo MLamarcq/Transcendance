f3619422115851f5de0372332ce78784199ebea7
Mlamarcq <mael.lamarcq0302@orange.fr>
2024-06-10 20:16:02 +0200

    premiere_page

--- backend/mysite/pong/views.py
+++ backend/mysite/pong/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "pong/homepage.html")
+    return render(request, "pong/index.html")
 
-
+def statistics(request):
+    return render(request, "pong/statistics.html")
